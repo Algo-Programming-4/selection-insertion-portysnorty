9e1eeec65a0d78ebe53ac29947631e4a7b2f468a
portysnorty <64874264+portysnorty@users.noreply.github.com>
2024-09-30 14:05:54 -0600

    Delete insertion.py

--- insertion.py
+++
@@ -1,9 +0,0 @@
-def insertion(ls):
-    for i in range(1,len(ls)):
-        j = i
-        while j > 0 and ls[j]<ls[j-1]:
-            temp = ls[j]
-            ls[j] = ls[j-1]
-            ls[j-1] = temp
-            j-=1
-    return ls
