cbcd3ace8ad3c442396d2cbd6419c5340c01b90d
portysnorty <64874264+portysnorty@users.noreply.github.com>
2024-09-30 14:04:51 -0600

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,4 +1,19 @@
 #bubble(list) - > sorted ist
+def bubble(ls):
+    for i in range(len(ls) - 1):  
+        # sorts it faster by checking if already sorted early
+        shortCut = True
+        for j in range(len(ls) - i - 1):  
+            a = ls[j]
+            b = ls[j + 1] 
+            if a > b:  
+                ls[j] = b 
+                ls[j + 1] = a
+                shortCut = False
+        if(shortCut):
+            print("saved you ",len(ls)-i-1," recurtions")
+            break
+    return ls
 
 #selection(list) -> sorted list
 def selection(ls):
