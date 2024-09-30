670497c840bdbfb89e16b747cd9d0311fc0eeea6
portysnorty <64874264+portysnorty@users.noreply.github.com>
2024-09-30 14:03:58 -0600

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -3,3 +3,12 @@
 #selection(list) -> sorted list
 
 #insertion(list) > sorted list
+def insertion(ls):
+    for i in range(1,len(ls)):
+        j = i
+        while j > 0 and ls[j]<ls[j-1]:
+            temp = ls[j]
+            ls[j] = ls[j-1]
+            ls[j-1] = temp
+            j-=1
+    return ls
