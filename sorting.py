dc24fbd7ee27c614552654ee36462b022a103e70
portysnorty <64874264+portysnorty@users.noreply.github.com>
2024-09-30 14:04:25 -0600

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,6 +1,18 @@
 #bubble(list) - > sorted ist
 
 #selection(list) -> sorted list
+def selection(ls):
+    for i in range(len(ls)-1):  
+        index_smallest = i
+        # finds the smallest index
+        for j in range(i+1, len(ls)):
+            if ls[index_smallest] > ls[j]:
+                index_smallest = j
+        
+        temp = ls[i]
+        ls[i] = ls[index_smallest]
+        ls[index_smallest] = temp
+    return ls
 
 #insertion(list) > sorted list
 def insertion(ls):
